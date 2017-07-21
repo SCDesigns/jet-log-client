cfb1969830722d91b57fb9dc51e5fc61599b90dd
Sean Clarke <xseanclarke@gmail.com>
2017-07-21 14:54:39 -0400

    Rendered Logs component successfully + staging for API call started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import Logs from './components/Logs'
+import LogService from './services/LogService'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      logs: []
+    }
+  }
+
+  componentDidMount(){
+    LogService.fetchLogs()
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Jet Log</h1>
+        <div className="navbar">
+          <Logs logs={this.state.logs}/>
+        </div>
+        <div className="map">
+          <p>Here</p>
+        </div>
       </div>
     );
   }
