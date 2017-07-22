14c09c91aeee236fad3c5277527d9b205b68b05c
Sean Clarke <xseanclarke@gmail.com>
2017-07-22 14:45:29 -0400

    fetch logs#index from Rails API working

--- src/services/LogService.js
+++ src/services/LogService.js
@@ -1,6 +1,8 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 const LogService = {
   fetchLogs: () => {
-    return fetch('/logs')
+    return fetch(`${API_URL}/logs`)
       .then(response => response.json())
   }
 }
