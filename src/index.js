eb4190c6ed1786b75b3c3860f7ed37adb8a996b7
Sean Clarke <xseanclarke@gmail.com>
2017-07-28 23:02:20 -0400

    Added Redux Provider and store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+
 import './index.css';
+import App from './App';
+import store from './redux/store'
+import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+  registerServiceWorker();
