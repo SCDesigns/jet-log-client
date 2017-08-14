da3e108e1d8a4b7435ab3d9b229315fa4ab57fe0
Sean Clarke <xseanclarke@gmail.com>
2017-08-14 14:49:44 -0400

    fixed missing log.title (was log.name)

--- src/views/Logs/LogCard.js
+++ src/views/Logs/LogCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const LogCard = ({ log }) => (
   <div key={log.id}>
-    <h3>{log.name}</h3>
+    <h3>{log.title}</h3>
     <p>{log.location}</p>
     <p>{log.description}</p>
     <img className="Memory" src={log.memory} alt={log.name} />
