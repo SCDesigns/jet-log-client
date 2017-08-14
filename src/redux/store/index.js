e63bdebbfd4a9fb4a44be0582026779b6fd89804
Sean Clarke <xseanclarke@gmail.com>
2017-08-14 11:52:39 -0400

    cleaned up index.html + renamed reducers and linked logs correctly

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,22 +5,19 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 // Reducers
-import { reducer as form } from 'redux-form';
 import authReducer from '../modules/Auth/reducer'
-import logs from '../modules/Logs/reducer'
+import logsReducer from '../modules/Logs/reducer'
 // import headerReducer from '../modules/Header/reducer'
 
-const middleware = [thunk]
-
 const reducers = combineReducers({
-  form,
   authReducer,
-  logs,
-  // headerReducer,
-})
+  logs: logsReducer,
+});
+
+const middleware = [thunk];
 
 export default createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(...middleware),
-)
+);
