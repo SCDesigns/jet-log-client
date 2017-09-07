2a334a6302f786628e8e134eabe20d0f16f819dc
Sean Clarke <xseanclarke@gmail.com>
2017-09-07 17:54:47 -0400

    like feature working w/ out persistence

--- src/views/Logs/Like.js
+++ src/views/Logs/Like.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import RaisedButton from 'material-ui/RaisedButton';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 class Like extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
 
-    this.state = {
-      like: 0
-    }
-
-    // this.handleOnChange = this.handleOnChange.bind(this)
+    this.state = { like: 0 }
+    this.increment = this.increment.bind(this)
+  }
+  increment(){
+    this.setState({ like: this.state.like + 1})
   }
 
-  // handleOnChange = (like) => {
-  //   this.setState({
-  //     like += 1;
+  // handleApiCall = () => {
+  //   console.log('a')
+  //
+  //   fetch(`${API_URL}/logs`)
+  //   .then(response => {
+  //     console.log('b')
+  //     return response.json()
   //   })
+  //   .then((logs) => {console.log(logs, 'c')})
+  //
+  //   console.log('d')
   // }
 
-  handleApiCall = () => {
-    console.log('a')
-
-    fetch(`${API_URL}/logs`)
-    .then(response => {
-      console.log('b')
-      return response.json()
-    })
-    .then((logs) => {console.log(logs, 'c')})
-
-    console.log('d')
-  }
   render(){
+    console.log('render')
     return (
       <div>
-        {/* <button onClick={this.handleOnChange}>Like</button> */}
-        <button onClick={this.handleApiCall}>Api Call</button>
-        <div>{this.state.like}</div>
+        {/* <button onClick={this.handleApiCall}>Api Call</button> */}
+        <button onClick={this.increment}>{this.state.like}</button>
       </div>
     )
   }
